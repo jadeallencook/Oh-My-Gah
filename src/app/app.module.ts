23e4e2aa1ba63e332a2518121b3a0c25f8d49595
Jade Allen Cook <jadeallencook@gmail.com>
2017-08-20 14:02:25 -0600

    🗄 Organzied CSS/JS dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,12 +11,27 @@ import { FirebaseAuthService } from './services/firebase-auth.service';
 
 import { RouterModule } from '@angular/router';
 
+// pages
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ItemComponent } from './pages/item/item.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
+
+// components
+import { BannerComponent } from './components/banner/banner.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ControlsComponent } from './components/controls/controls.component';
+import { DescriptionComponent } from './components/description/description.component';
+import { FeaturedComponent } from './components/featured/featured.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PaymentProcessComponent } from './components/payment-process/payment-process.component';
+import { PhotosComponent } from './components/photos/photos.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +40,19 @@ import { CheckoutComponent } from './pages/checkout/checkout.component';
     LoginComponent,
     DashboardComponent,
     ItemComponent,
-    CheckoutComponent
+    CheckoutComponent,
+    BannerComponent,
+    BlogComponent,
+    ControlsComponent,
+    DescriptionComponent,
+    FeaturedComponent,
+    FeaturesComponent,
+    FooterComponent,
+    NavbarComponent,
+    PaymentProcessComponent,
+    PhotosComponent,
+    ShoppingCartComponent,
+    UserDetailsComponent
   ],
   imports: [
     BrowserModule,
