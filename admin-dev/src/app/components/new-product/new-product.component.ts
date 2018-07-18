bf1ffa2b59541faa7acb9e160a205d2034d2d6b4
Jade Allen Cook <jadeallencook@gmail.com>
2018-07-18 15:25:52 -0600

    🏁 Completed products page for dashboard

--- admin-dev/src/app/components/new-product/new-product.component.ts
+++ admin-dev/src/app/components/new-product/new-product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import * as firebase from 'firebase';
 
 @Component({
@@ -7,6 +8,7 @@ import * as firebase from 'firebase';
   styleUrls: ['./new-product.component.css']
 })
 export class NewProductComponent implements OnInit {
+
   item = {
     title: null,
     stock: null,
@@ -20,14 +22,92 @@ export class NewProductComponent implements OnInit {
     image: null
   }
   image: any;
-  base64: string;
-  constructor() { }
+  items = {};
+  uids = [];
+  images = {};
+  editor = {
+    edit: false,
+    selected: null
+  }
+
+  constructor(private router: Router) {
+    firebase.database().ref('products/').on('value', (snapshot) => {
+      this.items = snapshot.val();
+      this.uids = Object.keys(this.items);
+      this.uids.forEach(uid => {
+        var image = this.items[uid].image;
+        firebase.storage().refFromURL('gs://oh-my-gah.appspot.com/' + image).getDownloadURL().then((url) => {
+          this.images[uid] = url;
+        });
+      });
+    });
+  }
 
   add() {
-    console.log(this.image);
+    var timestamp = new Date().getTime(),
+      ref = firebase.storage().ref(timestamp + '.jpeg'),
+      imageElem = document.getElementById('base64').children[0],
+      test = true;
+    if (imageElem) {
+      document.getElementById('image-label').style.color = '#767676';
+      this.item.image = timestamp + '.jpeg';
+      Object.keys(this.item).forEach(key => {
+        var elem = document.getElementById(key + '-label');
+        if (elem && !this.item[key]) {
+          elem.style.color = '#FF0000';
+          test = false;
+        } else if (elem && this.item[key]) {
+          elem.style.color = '#767676';
+        }
+      });
+      if (test) {
+        ref.putString(imageElem.getAttribute('src').toString(), 'data_url').then(() => {
+          firebase.database().ref('/products/').push(this.item).then(() => {
+            this.reset();
+          });
+        });
+      }
+    } else {
+      document.getElementById('image-label').style.color = '#FF0000';
+    }
   }
 
-  ngOnInit() {
+  edit(uid) {
+    this.editor.edit = true;
+    this.editor.selected = uid;
+    this.item = this.items[uid];
   }
+
+  reset() {
+    this.router.navigate(['/', 'products']);
+    this.item = {
+      title: null,
+      stock: null,
+      price: null,
+      id: null,
+      description: null,
+      featured: false,
+      front: false,
+      email: false,
+      sale: false,
+      image: null
+    }
+    this.editor.edit = false;
+    this.editor.selected = null;
+    var elem = document.getElementById('remove-photo');
+    if (elem) elem.click();
+  }
+
+  save() {
+    firebase.database().ref('products/' + this.editor.selected).set(this.item).then(() => {
+      this.reset();
+    });
+  }
+
+  delete(uid) {
+    firebase.database().ref('products/').child(uid).remove();
+  }
+
+  ngOnInit() { }
 
 }
